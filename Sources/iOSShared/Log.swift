c9da1a5cbcc32161a6ca6f319a7551595d1d7ef4
Christopher Prince <chris@SpasticMuffin.biz>
2020-08-29 11:18:14 -0600

    Add comment.

--- Sources/iOSShared/Log.swift
+++ Sources/iOSShared/Log.swift
@@ -1,3 +1,4 @@
 import Logging
 
+// I'd really like to centralize this Logger instantiation. See https://github.com/apple/swift-log/issues/155
 public let logger = Logger(label: "com.SpasticMuffin.Neebla")
