8a0dc04110cc0cf8f4f17ec9e207825e3f4eea5b
Christopher Prince <chris@SpasticMuffin.biz>
2020-09-05 20:23:25 -0600

    Enable log level to be set.

--- Sources/iOSShared/Log.swift
+++ Sources/iOSShared/Log.swift
@@ -1,4 +1,8 @@
 import Logging
 
 // I'd really like to centralize this Logger instantiation. See https://github.com/apple/swift-log/issues/155
-public let logger = Logger(label: "com.SpasticMuffin.Neebla")
+public private(set) var logger = Logger(label: "com.SpasticMuffin.Neebla")
+
+public func set(logLevel: Logging.Logger.Level) {
+    logger.logLevel = logLevel
+}
