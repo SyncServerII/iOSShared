b278cf9ab58c6deecc276b8cc12665fa7063b2c6
Christopher Prince <chris@SpasticMuffin.biz>
2020-10-01 22:14:10 -0600

    Improving set method for logging.

--- Sources/iOSShared/Log.swift
+++ Sources/iOSShared/Log.swift
@@ -1,8 +1,9 @@
 import Logging
 
 // I'd really like to centralize this Logger instantiation. See https://github.com/apple/swift-log/issues/155
-public private(set) var logger = Logger(label: "com.SpasticMuffin.Neebla")
+public private(set) var logger = Logger(label: "")
 
-public func set(logLevel: Logging.Logger.Level) {
+public func set(logLevel: Logging.Logger.Level, logLabel: String = "") {
+    logger = Logger(label: logLabel)
     logger.logLevel = logLevel
 }
