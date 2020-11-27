bee24412b3eb8dd46b026cfb43357b0b3d12d713
Christopher Prince <chris@SpasticMuffin.biz>
2020-11-26 20:47:36 -0700

    Add throwing/rethrowing to Synchronized.

--- Sources/iOSShared/Synchronized.swift
+++ Sources/iOSShared/Synchronized.swift
@@ -4,9 +4,9 @@ import Foundation
 // From http://stackoverflow.com/questions/24045895/what-is-the-swift-equivalent-to-objective-cs-synchronized
 
 open class Synchronized {
-    open class func block(_ lock: AnyObject, closure: () -> ()) {
+    open class func block(_ lock: AnyObject, closure: () throws -> ()) rethrows {
         objc_sync_enter(lock)
-        closure()
+        try closure()
         objc_sync_exit(lock)
     }
 }
