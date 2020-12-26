0f924575fa4a453b062435d2a6fc3c636add7c2e
Christopher Prince <chris@SpasticMuffin.biz>
2020-12-25 20:33:21 -0700

    Added UserAlertModel and related.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "iOSShared",
     platforms: [
-        .iOS(.v9),
+        // To support SwiftUI
+        .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
---
+++ Sources/iOSShared/UserAlertModel.swift
@@ -0,0 +1,28 @@
+
+import Foundation
+import SwiftUI
+
+public enum UserAlertContents {
+    case title(String)
+    case full(title: String, message: String)
+    
+    // Shows "Error" as title
+    case error(message: String)
+}
+
+public protocol UserAlertMessage: AnyObject {
+    var userAlert: UserAlertContents? { get set }
+    var screenDisplayed: Bool { get set }
+}
+
+public class UserAlertModel: ObservableObject, UserAlertMessage {
+    public var screenDisplayed: Bool = false
+    @Published public var show: Bool = false
+    
+    public var userAlert: UserAlertContents? {
+        didSet {
+            // If we don't constrain this by whether or not the screen is displayed, when we navigate to other screens, we get the same error, once per screen-- because each screen model has an `errorSubscription`. I don't know if having these models remain allocated is standard behavior for SwiftUI, but currently it is the case.
+            show = userAlert != nil && screenDisplayed
+        }
+    }
+}
