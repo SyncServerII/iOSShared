0fa8a3c7db907362b51c0b7259ee3cd72d4a44cf
Christopher Prince <chris@SpasticMuffin.biz>
2020-12-25 21:07:31 -0700

    Added comment.

--- Sources/iOSShared/UserAlertModel.swift
+++ Sources/iOSShared/UserAlertModel.swift
@@ -2,6 +2,8 @@
 import Foundation
 import SwiftUI
 
+// User Alert Messages are intended to be informational, and not require a decision on the part of the user. They can be positive (e.g., "A user was created." or negative (e.g. "A server request failed.").
+
 public enum UserAlertContents {
     case title(String)
     case full(title: String, message: String)
