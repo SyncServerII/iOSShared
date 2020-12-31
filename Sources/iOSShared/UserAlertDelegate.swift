96a56bf20979217704590fae7aa77e0352b1e75e
Christopher Prince <chris@SpasticMuffin.biz>
2020-12-30 22:14:36 -0700

    Added customAction to UserAlertContents and changed case names

--- Sources/iOSShared/UserAlertDelegate.swift
+++ Sources/iOSShared/UserAlertDelegate.swift
@@ -1,14 +1,18 @@
 
 import Foundation
 
-// User Alert Messages are intended to be informational, and not require a decision on the part of the user. They can be positive (e.g., "A user was created." or negative (e.g. "A server request failed.").
+// User Alert Messages are intended to be informational; most not require a decision on the part of the user. They can be positive (e.g., "A user was created." or negative (e.g. "A server request failed.").
 
 public enum UserAlertContents {
-    case title(String)
-    case full(title: String, message: String)
+    // Only OK button
+    case titleOnly(String)
+    case titleAndMessage(title: String, message: String)
     
     // Shows "Error" as title
     case error(message: String)
+    
+    // With custom action button-- "Cancel" is also given.
+    case customAction(title: String, message: String, actionButtonTitle:String, action:()->())
 }
 
 public protocol UserAlertDelegate: AnyObject {
