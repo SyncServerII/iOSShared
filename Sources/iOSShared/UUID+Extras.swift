9897d4513ddad6e9f05fe17d11fade790fabd22f
Christopher Prince <chris@SpasticMuffin.biz>
2021-01-09 18:08:54 -0700

    Added logging.

--- Sources/iOSShared/UUID+Extras.swift
+++ Sources/iOSShared/UUID+Extras.swift
@@ -9,6 +9,7 @@ extension UUID {
     public static func from(_ string: String?) throws -> UUID? {
         if let string = string {
             guard let uuid = UUID(uuidString: string) else {
+                logger.error("badUUIDString: \(string)")
                 throw UUIDError.badUUIDString
             }
             return uuid
