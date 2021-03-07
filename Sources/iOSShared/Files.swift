cafa52987de85d9ff2badd3e052893cad36c04c9
Christopher Prince <chris@SpasticMuffin.biz>
2021-03-06 21:32:44 -0700

    Marking as excluded from backup when creating a directory.

--- Sources/iOSShared/Files.swift
+++ Sources/iOSShared/Files.swift
@@ -9,16 +9,10 @@ public struct Files {
     
     // Creates a temporary file within the given directory. If the URL returned, the file will have been created, and zero length upon return. Uses a UUID to form the file name and assumes there will be no collision.
     // If create is false, doesn't actually create the file. But makes sure the file doesn't already exist.
+    // Creates the directory if needed. If created, marks the directory as excluded from (e.g., iCloud) backup.
     public static func createTemporary(withPrefix prefix: String, andExtension extension: String, inDirectory directory: URL, create: Bool = true) throws -> URL {
-        var directory = directory
         try createDirectoryIfNeeded(directory)
-        
-        // Don't let these temporary files be backed up to iCloud-- Apple doesn't like this (e.g., when reviewing apps).
-        
-        var resourceValues = URLResourceValues()
-        resourceValues.isExcludedFromBackup = true
-        try directory.setResourceValues(resourceValues)
-        
+
         let uuid = UUID().uuidString
         let fileName = "\(prefix).\(uuid).\(`extension`)"
         
@@ -37,8 +31,9 @@ public struct Files {
         
         return newFileURL
     }
-    
-    public static func createDirectoryIfNeeded(_ directory: URL) throws {
+
+    // If `excludeFromBackup` is true, and the directory is created, don't let these temporary files be backed up to iCloud-- Apple doesn't like this (e.g., when reviewing apps).
+    public static func createDirectoryIfNeeded(_ directory: URL, excludeFromBackup: Bool = true) throws {
         var isDir : ObjCBool = false
         if FileManager.default.fileExists(atPath: directory.path, isDirectory:&isDir) {
             if !isDir.boolValue {
@@ -47,6 +42,13 @@ public struct Files {
         } else {
             // Create the directory.
             try FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil)
+            
+            if excludeFromBackup {
+                var directory = directory
+                var resourceValues = URLResourceValues()
+                resourceValues.isExcludedFromBackup = true
+                try directory.setResourceValues(resourceValues)
+            }
         }
     }
     
