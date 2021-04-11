0c4c943b519be9be59994299dd0bfdf232042acc
Christopher Prince <chris@SpasticMuffin.biz>
2021-04-11 17:02:40 -0600

    Enabling alert messages to be queued

--- Sources/iOSShared/Alerty/AlertyDisplayer.swift
+++ Sources/iOSShared/Alerty/AlertyDisplayer.swift
@@ -12,19 +12,18 @@ extension View {
     public func alertyDisplayer(show: Binding<Bool>, subscriber: AlertySubscriber) -> some View {
         self
         .alert(isPresented: show) {
-            logger.debug("alertyDisplayer: Showing: \(String(describing: subscriber.debugMessage))")
-            return subscriber.getAlert()
+            let result = subscriber.getAlert()
+            logger.debug("About to show: \(result)")
+            return result
         }
         .onAppear() {
-            logger.debug("onAppear: \(String(describing: subscriber.debugMessage))")
             subscriber.screenDisplayed = true
         }
         .onDisappear() {
-            logger.debug("onDisappear: \(String(describing: subscriber.debugMessage))")
             subscriber.screenDisplayed = false
             
             // I had been getting duplicate error messages. e.g., on sharing account creation by an invitation. This is to avoid this.
-            subscriber.alert = nil
+            subscriber.alertQueue.removeAll()
         }
     }
 }
--- Sources/iOSShared/Alerty/AlertySubscriber.swift
+++ Sources/iOSShared/Alerty/AlertySubscriber.swift
@@ -11,14 +11,14 @@ import Combine
 
 public class AlertySubscriber: ObservableObject {
     var subscription:AnyCancellable!
-    var alert: AlertyContents? {
+    var alertQueue: [AlertyContents] = [] {
         didSet {
-            // If we don't constrain this by whether or not the screen is displayed, when we navigate to other screens, we get the same error, once per screen-- because each screen model has an `errorSubscription`. I don't know if having these models remain allocated is standard behavior for SwiftUI, but currently it is the case.
-            let shouldShow = alert != nil && screenDisplayed
-            logger.debug("didSet userAlert: shouldShow: \(shouldShow); \(String(describing: debugMessage)): userAlert: \(String(describing: self.alert)): screenDisplayed: \(screenDisplayed)")
-            if self.show != shouldShow {
-                DispatchQueue.main.async {
-                    self.show = shouldShow
+            // Execute all of the below on the main queue to avoid race conditions with setting show to true.
+            DispatchQueue.main.async {
+                // If we don't constrain this by whether or not the screen is displayed, when we navigate to other screens, we get the same error, once per screen-- because each screen model has an `errorSubscription`. I don't know if having these models remain allocated is standard behavior for SwiftUI, but currently it is the case.
+                let shouldShow = self.alertQueue.count > 0 && self.screenDisplayed && !self.show
+                if shouldShow {
+                    self.show = true
                 }
             }
         }
@@ -26,21 +26,35 @@ public class AlertySubscriber: ObservableObject {
     
     let debugMessage: String?
     public var screenDisplayed = false
-    @Published public var show = false
+    @Published public var show = false {
+        didSet {
+            // Doing a dispatch here as there seems to be need to be a delay to actually display the next message if there is one. And to avoid a race condition in setting `show`.
+            DispatchQueue.main.async {
+                let shouldShowNext = self.alertQueue.count > 0 && self.screenDisplayed && !self.show
+                logger.debug("shouldShowNext: \(shouldShowNext), alertQueue.count: \(self.alertQueue.count), screenDisplayed: \(self.screenDisplayed), show: \(self.show)")
+                if shouldShowNext {
+                    self.show = true
+                }
+            }
+        }
+    }
 
     public init(debugMessage: String? = nil, publisher: AlertyPublisher) {
         self.debugMessage = debugMessage
         
         subscription = publisher.alerty.sink(receiveValue: { [weak self] contents in
             guard let self = self else { return }
-            logger.debug("debugMessage: \(String(describing: debugMessage)); sink: \(String(describing: contents))")
-            self.alert = contents
+            if let contents = contents, self.screenDisplayed {
+                self.alertQueue.append(contents)
+            }
         })
     }
     
     func getAlert() -> SwiftUI.Alert {
-        if let alert = alert {
-            return alert.alert
+        if alertQueue.count > 0 {
+            let head = alertQueue[0]
+            alertQueue.removeFirst()
+            return head.alert
         }
         else {
             logger.error("Unknown alert")
