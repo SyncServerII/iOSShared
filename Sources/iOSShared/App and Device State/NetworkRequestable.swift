b465e86931fe0b8531601041534554d97355d2bb
Christopher Prince <chris@SpasticMuffin.biz>
2021-04-15 19:21:49 -0600

    version 0.12.0

--- Sources/iOSShared/App and Device State/NetworkRequestable.swift
+++ Sources/iOSShared/App and Device State/NetworkRequestable.swift
@@ -7,7 +7,23 @@
 
 import Foundation
 
+public struct NetworkRequestableOptions: OptionSet {
+    public let rawValue: Int
+
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+  
+    public static let network = NetworkRequestableOptions(rawValue: 1 << 0)
+    public static let app = NetworkRequestableOptions(rawValue: 1 << 1)
+    
+    public static let all:NetworkRequestableOptions = [.network, .app]
+}
+
 public protocol NetworkRequestable {
     // Is the app and the network in a state where network requests can be made?
     var canMakeNetworkRequests: Bool { get }
+    
+    // A more fine grained check than the Bool property.
+    func canMakeNetworkRequests(options:NetworkRequestableOptions) -> Bool
 }
