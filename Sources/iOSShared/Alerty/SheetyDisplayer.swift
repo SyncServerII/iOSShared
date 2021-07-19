4b732b4915f51ab83d1c54ccb3ad6854a968084f
Christopher Prince <chris@SpasticMuffin.biz>
2021-07-18 22:59:53 -0600

    version 0.15.0

--- Sources/iOSShared/Alerty/SheetyDisplayer.swift
+++ Sources/iOSShared/Alerty/SheetyDisplayer.swift
@@ -11,20 +11,22 @@ import SwiftUI
 // Because I'm not getting the callbacks for displaying/hiding view when displaying a sheet. And because I want to display errors/alerts on these sheets.
 
 extension View {
-    public func sheetyDisplayer<V: View>(show: Binding<Bool>, subscriber: AlertySubscriber, view: V) -> some View {
+    public func sheetyDisplayer<V: View>(show: Binding<Bool>, subscriber: AlertySubscriber, onDismiss: (() -> Void)? = nil, view: V) -> some View {
     
         self.sheet(isPresented: show, onDismiss: {
             subscriber.screenDisplayed = true
+            onDismiss?()
         }, content: { ()->(V) in
             subscriber.screenDisplayed = false
             return view
         })
     }
     
-    public func sheetyDisplayer<Item, Content>(item: Binding<Item?>, subscriber: AlertySubscriber, @ViewBuilder content: @escaping (Item) -> Content) -> some View where Item : Identifiable, Content : View {
+    public func sheetyDisplayer<Item, Content>(item: Binding<Item?>, subscriber: AlertySubscriber, onDismiss: (() -> Void)? = nil, @ViewBuilder content: @escaping (Item) -> Content) -> some View where Item : Identifiable, Content : View {
     
         self.sheet(item: item, onDismiss: {
             subscriber.screenDisplayed = true
+            onDismiss?()
         }, content: { (item: Item) -> Content in
             subscriber.screenDisplayed = false
             return content(item)
